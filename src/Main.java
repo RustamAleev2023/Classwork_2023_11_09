ffda4fe08f61ca8c343df14f7849e02b9e17fdab
RustamAleev2023 <ridermix@yandex.ru>
2023-11-09 19:26:10 +0200

    Task1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import entity.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    //Task1
+
+        Person person1 = new Person("name1", "01.01.2000", "123-12-12", "Kaliningrad", "Russia", "Mira 5-5");
+        System.out.println(person1);
+        System.out.println("Counter = " + Person.counter());
+        Person person2= new Person("name2", "01.01.2000", "123-12-12", "Kaliningrad", "Russia", "Mira 5-5");
+        System.out.println(person2);
+        System.out.println("Counter = " + Person.counter());
+        Person person3 = new Person("name3", "01.01.2000", "123-12-12", "Kaliningrad", "Russia", "Mira 5-5");
+        System.out.println(person2);
+        System.out.println("Counter = " + Person.counter());
+        Person person4 = new Person("name4", "01.01.2000", "123-12-12", "Kaliningrad", "Russia", "Mira 5-5");
+        System.out.println(person4);
+        System.out.println("Counter = " + Person.counter());
+        Person person5 = new Person("name5", "01.01.2000", "123-12-12", "Kaliningrad", "Russia", "Mira 5-5");
+        System.out.println(person5);
+        System.out.println("Counter = " + Person.counter());
+
+
     }
 }
---
+++ src/entity/Person.java
@@ -0,0 +1,84 @@
+package entity;
+
+public class Person {
+    String fullName;
+    String birthday;
+    String phone;
+
+    String city;
+    String country;
+    String homeAddress;
+    static int counter = 0;
+
+    public Person(String fullName, String birthday, String phone, String city, String country, String homeAddress) {
+        this.fullName = fullName;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.city = city;
+        this.country = country;
+        this.homeAddress = homeAddress;
+        counter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public static int counter(){
+        return counter;
+    }
+
+    @Override
+    public String toString() {
+        return "Person " + "fullName: " + fullName
+                + ", birthday: " + birthday
+                + ", phone: " + phone
+                + ", city: " + city
+                + ", country: " + country
+                + ", homeAddress: " + homeAddress;
+    }
+}
